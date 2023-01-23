9041b04ffc4f61d85e4b5e026812c8caa57fe60c
Yashasv <joshiyashu10@gmail.com>
2023-01-23 20:43:01 +0530

    bye

---
+++ Frontend/logic-js/script.js
@@ -0,0 +1,70 @@
+// let aboutElementArrow = document.getElementById("a-arrow");
+// let serviceElementArrow = document.getElementById("s-arrow");
+// let aboutDropdown = document.getElementById("a-dropdown");
+// let servicesDropdown = document.getElementById("s-dropdown");
+// // showAboutDropdown();
+
+// const showAboutDropdown = () => {
+//   aboutDropdown.style.visibility = "visible";
+// };
+
+// const showServicesDropdown = () => {
+//   servicesDropdown.style.visibility = "visible";
+// };
+
+// // const toggleVisiblity = (element) => {
+// //   if ((element.style.visibility = "hidden")) {
+// //     element.style.visibility = "visible";
+// //     "visible");
+// //   } else if ((element.style.visibility = "visible")) {
+// //     element.style.visibility = "hidden";
+// //     "hidden");
+// //   } else {
+// //     "dfui");
+// //   }
+// //   // element.style.visibility = 'visible'
+// // };
+
+// aboutElementArrow.addEventListener("mouseover", () => {
+//   aboutDropdown.style.visibility = "visible";
+// });
+// aboutElementArrow.addEventListener("mouseout", () => {
+//   // setInterval(() => {
+//   aboutDropdown.style.visibility = "hidden";
+//   // }, 2200);
+// });
+
+// aboutDropdown.addEventListener("mouseover", () => {
+//   aboutDropdown.style.visibility = "visible";
+// });
+// aboutDropdown.addEventListener("mouseout", () => {
+//   setInterval(() => {
+//     aboutDropdown.style.visibility = "hidden";
+//   }, 1200);
+// });
+
+// serviceElementArrow.addEventListener("mouseover", () => {
+//   servicesDropdown.style.visibility = "visible";
+// });
+
+// serviceElementArrow.addEventListener("mouseout", () => {
+//   // setInterval(() => {
+//   if (servicesDropdown.style.visibility == "hidden") return;
+//   else {
+//     servicesDropdown.style.visibility = "visible";
+//   }
+//   setInterval(() => {
+//     aboutDropdown.style.visibility = "hidden";
+//   }, 1200);
+//   // }, 1200);
+// });
+
+// servicesDropdown.addEventListener("mouseover", () => {
+//   servicesDropdown.style.visibility = "visible";
+// });
+// servicesDropdown.addEventListener("mouseout", () => {
+//   setInterval(() => {
+//     servicesDropdown.style.visibility = "hidden";
+//   }, 1200);
+// });
+// // serviceElementArrow.addEventListener("mouseover", ()=);
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./Frontend/**/*.{html,js,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
