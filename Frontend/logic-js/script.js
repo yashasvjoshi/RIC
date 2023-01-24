dce96a962f4d5f7bb568d6502478201dbd3d6bd1
Yashasv <joshiyashu10@gmail.com>
2023-01-24 22:53:02 +0530

    Doing fine

--- Frontend/logic-js/script.js
+++ Frontend/logic-js/script.js
@@ -1,70 +1,9 @@
-// let aboutElementArrow = document.getElementById("a-arrow");
-// let serviceElementArrow = document.getElementById("s-arrow");
-// let aboutDropdown = document.getElementById("a-dropdown");
-// let servicesDropdown = document.getElementById("s-dropdown");
-// // showAboutDropdown();
+let ptitle = "RAJYASH IT CONSULTENCY ";
+let path = document.location.pathname.split("/");
 
-// const showAboutDropdown = () => {
-//   aboutDropdown.style.visibility = "visible";
-// };
-
-// const showServicesDropdown = () => {
-//   servicesDropdown.style.visibility = "visible";
-// };
-
-// // const toggleVisiblity = (element) => {
-// //   if ((element.style.visibility = "hidden")) {
-// //     element.style.visibility = "visible";
-// //     "visible");
-// //   } else if ((element.style.visibility = "visible")) {
-// //     element.style.visibility = "hidden";
-// //     "hidden");
-// //   } else {
-// //     "dfui");
-// //   }
-// //   // element.style.visibility = 'visible'
-// // };
-
-// aboutElementArrow.addEventListener("mouseover", () => {
-//   aboutDropdown.style.visibility = "visible";
-// });
-// aboutElementArrow.addEventListener("mouseout", () => {
-//   // setInterval(() => {
-//   aboutDropdown.style.visibility = "hidden";
-//   // }, 2200);
-// });
-
-// aboutDropdown.addEventListener("mouseover", () => {
-//   aboutDropdown.style.visibility = "visible";
-// });
-// aboutDropdown.addEventListener("mouseout", () => {
-//   setInterval(() => {
-//     aboutDropdown.style.visibility = "hidden";
-//   }, 1200);
-// });
-
-// serviceElementArrow.addEventListener("mouseover", () => {
-//   servicesDropdown.style.visibility = "visible";
-// });
-
-// serviceElementArrow.addEventListener("mouseout", () => {
-//   // setInterval(() => {
-//   if (servicesDropdown.style.visibility == "hidden") return;
-//   else {
-//     servicesDropdown.style.visibility = "visible";
-//   }
-//   setInterval(() => {
-//     aboutDropdown.style.visibility = "hidden";
-//   }, 1200);
-//   // }, 1200);
-// });
-
-// servicesDropdown.addEventListener("mouseover", () => {
-//   servicesDropdown.style.visibility = "visible";
-// });
-// servicesDropdown.addEventListener("mouseout", () => {
-//   setInterval(() => {
-//     servicesDropdown.style.visibility = "hidden";
-//   }, 1200);
-// });
-// // serviceElementArrow.addEventListener("mouseover", ()=);
+if (path[3] == "contact.html") {
+  // console.log(path[3]);
+  document.title = ptitle + "-Contact Us";
+} else if (path[3] == "index.html") {
+  document.title = ptitle;
+}
